c620e8ca909bbdd1bf43e7bbf0055b63f29234ca
developerbsm <sebastianbastidaofficial@gmail.com>
2025-04-08 21:32:45 -0700

    Fixed eslint

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -9,6 +9,7 @@ const eslintConfig = [
     extends: ['next/core-web-vitals', 'next/typescript'],
     rules: {
       '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
       'react/no-unescaped-entities': 'off',
       'prefer-const': 'off',
     },
